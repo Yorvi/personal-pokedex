56ae7b95f2af31245ff8787930063b4422bcf02e
Yorvi <jorvinrosario7@gmail.com>
2018-04-03 16:25:21 -0400

    First Branch Commit

--- js/abra.js
+++ js/abra.js
@@ -1,9 +1,9 @@
-document.getElementById("abraPic").addEventListener('mouseover', function() {
-	document.getElementById("abraPic").src = "images/shiny-abra.jpg"
-})
+// document.getElementById("abraPic").addEventListener('mouseover', function() {
+// 	document.getElementById("abraPic").src = "images/shiny-abra.jpg"
+// })
 
-let ul = document.querySelector("ul")
 
+// I create a class called pokemon and store its id#, name, hp, attack, defense, and abilities to be filled in later.
 class Pokemon {
 	constructor(index, pokemon, hp, attack, defense, abilities) {
 		this.index = index;
@@ -15,10 +15,12 @@ class Pokemon {
 	}
 }
 
-axios.get("https://pokeapi.co/api/v2/pokemon/63/")
-.then((response) => {
+// Axios gets the source of the info im trying to relay onto my page.
+axios.get("https://pokeapi.co/api/v2/pokemon/63/").then((response) => {
+
+	// Im grabbing a bunch of tags by an id or class that I want to fill with the info from the API.
 	let data = response.data
-	let section = document.querySelector("#pokeInfo2")
+	
 	let index = document.querySelector("#id2")
 	let name = document.querySelector("#name2")
 	let hp = document.querySelector("#hp2")
@@ -29,7 +31,9 @@ axios.get("https://pokeapi.co/api/v2/pokemon/63/")
 	let abl3 = document.querySelector(".abl3")
 	let abl4 = document.querySelector(".abl4")
 
+	// Here I ceate a new pokemon to fill in the info in the class Pokemon with API info.
 	let abra = new Pokemon (
+		// Here I use the info I linked to the class and use it to display it in the HTML file with the querySelector that I grabbed earlier.
 		data.id,
 		data.name,
 		data.stats[5].base_stat,
@@ -40,6 +44,8 @@ axios.get("https://pokeapi.co/api/v2/pokemon/63/")
 		data.abilities[2].ability.name,
 		data.moves[51].move.name,
 	)
+
+	// Here I use the info I linked to the class and use it to display it in the HTML file with the querySelector that I grabbed earlier.
 	index.innerHTML = "#0" + data.id
 	name.innerHTML = data.name
 	hp.innerHTML = "HP: " + data.stats[5].base_stat
@@ -50,7 +56,6 @@ axios.get("https://pokeapi.co/api/v2/pokemon/63/")
 	abl3.innerHTML = data.abilities[2].ability.name
 	abl4.innerHTML = data.moves[51].move.name
 
-})
-.catch((error) => {
+}).catch((error) => {
   console.log(error)
 })
