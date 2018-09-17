c0cec7f79de5aa552aa319443f5e6e3f0f262273
Yorvi <jorvinrosario7@gmail.com>
2018-09-17 01:30:22 -0400

    added the correct axios call and fixed bugs, new bug arrived: id doesnt get rewritten when you want a new pokemon just adds on the number

--- js/main.js
+++ js/main.js
@@ -184,57 +184,70 @@
 //   console.log(error)
 // })
 
-let search = "";
+let input = "";
+let button = document.getElementById("button");
+let apiUrl = "https://pokeapi.co/api/v2/pokemon/";
 
 function capFirstLet(string) {
   string.charAt(0).toUpperCase() + string.slice(1);
-
   return string
 }
 
+function callApi() {
+  axios.get(apiUrl).then(response => {
+    let data = response.data;
 
-function storeVal() {
-  search = document.getElementById( "search-input" ).value;
+    let name = document.getElementById("pkmn-name");
+    let index = document.getElementById("pkmn-id");
+    let pkmnImg = document.getElementById("pkmn-sprite");
+    let height = document.getElementById("pkmn-height");
+    let weight = document.getElementById("pkmn-weight");
+    let types = document.getElementById("pkmn-type");
+    let abl1 = document.getElementById("abl1");
+    let abl2 = document.getElementById("abl2");
+    let abl3 = document.getElementById("abl3");
+    let abl4 = document.getElementById("abl4");
 
-  if ( !isNaN( search ) == true ) {
-    console.log( search );
-  } else {
-    alert("Pokémon ID is either not a valid number, not available, or does not exist, please try another number.");
-  }
-}
+    name.innerHTML = data.name.charAt(0).toUpperCase() + data.name.slice(1);
 
+    if ( 0 < data.id && data.id < 10 ) {
+      index.innerHTML = "#00" + data.id;
+    };
+    if ( 9 < data.id && data.id < 100 ) {
+      index.innerHTML = "#0" + data.id;
+    };
+    if ( 99 < data.id ) {
+      index.innerHTML = "#" + data.id;
+    };
 
-axios.get("https://pokeapi.co/api/v2/pokemon/3").then(response => {
-  let data = response.data;
+    pkmnImg.innerHTML =
+      "<img src='" + data.sprites.front_default + "' id='pkmn-img'>";
+    height.innerHTML = "Height: " + data.height + "0 cm";
+    weight.innerHTML = "Weight: " + data.weight + " kg";
+    types.innerHTML =
+      "Type: " +
+      data.types[0].type.name.charAt(0).toUpperCase() +
+      data.types[0].type.name.slice(1);
 
-  let name = document.getElementById("pkmn-name");
-  let index = document.getElementById("pkmn-id");
-  let pkmnImg = document.getElementById("pkmn-sprite");
-  let height = document.getElementById("pkmn-height");
-  let weight = document.getElementById("pkmn-weight");
-  let type = document.getElementById("pkmn-type");
-  let abl1 = document.getElementById("abl1");
-  let abl2 = document.getElementById("abl2");
-  let abl3 = document.getElementById("abl3");
-  let abl4 = document.getElementById("abl4");
-
-
-
-  name.innerHTML = data.name.charAt(0).toUpperCase() + data.name.slice(1);
-  index.innerHTML = "#00" + data.id;
-  pkmnImg.innerHTML = "<img src='" + data.sprites.front_default + "' id='pkmn-img'>"
-  height.innerHTML = "Height: " + data.height + "0 cm";
-  weight.innerHTML = "Weight: " + data.weight + " kg";
-  type.innerHTML = "Type: " + data.types[0].type.name.charAt(0).toUpperCase() + data.types[0].type.name.slice(1);
-  abl1.innerHTML = data.moves[3].move.name.charAt(0).toUpperCase() + data.moves[3].move.name.slice(1);
-  abl2.innerHTML = data.moves[2].move.name.charAt(0).toUpperCase() + data.moves[2].move.name.slice(1);
-  abl3.innerHTML = data.moves[7].move.name.charAt(0).toUpperCase() + data.moves[7].move.name.slice(1);
-  abl4.innerHTML = data.moves[6].move.name.charAt(0).toUpperCase() + data.moves[6].move.name.slice(1);
-
-
-
-
-
-}).catch(error => {
+    abl1.innerHTML =
+      data.moves[3].move.name.charAt(0).toUpperCase() +
+      data.moves[3].move.name.slice(1);
+    abl2.innerHTML =
+      data.moves[2].move.name.charAt(0).toUpperCase() +
+      data.moves[2].move.name.slice(1);
+    abl3.innerHTML =
+      data.moves[7].move.name.charAt(0).toUpperCase() +
+      data.moves[7].move.name.slice(1);
+    abl4.innerHTML =
+      data.moves[6].move.name.charAt(0).toUpperCase() +
+      data.moves[6].move.name.slice(1);
+  }).catch(error => {
     console.log(error);
   });
+}
+
+button.addEventListener("click", () => {
+  input = document.getElementById("search-input").value;
+  apiUrl = apiUrl + input;
+  callApi();
+});
