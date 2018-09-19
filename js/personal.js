6a110a2082f94a6cf040977dfb18cc2f4204942e
Yorvi <jorvinrosario7@gmail.com>
2018-09-19 02:46:39 -0400

    savepoint

--- js/personal.js
+++ js/personal.js
@@ -35,17 +35,18 @@ axios.get("https://pokeapi.co/api/v2/pokemon/59/").then((response) => {
 
   let pkmnImg = document.getElementById("my-pkmn1");
 
-  // let arcanine = new Pokemon(
-  //   data.id,
-  //   data.name,
-  //   data.stats[5].base_stat,
-  //   data.stats[4].base_stat,
-  //   data.stats[3].base_stat,
-  //   data.abilities[0].ability.name,
-  //   data.abilities[1].ability.name,
-  //   data.abilities[2].ability.name,
-  //   data.moves[7].move.name,
-  // )
+  let arcanine = new Pokemon(
+    data.id,
+    data.name,
+    data.stats[5].base_stat,
+    data.stats[4].base_stat,
+    data.stats[3].base_stat,
+
+    data.moves[0].move.name,
+    data.moves[1].move.name,
+    data.moves[2].move.name,
+    data.moves[7].move.name,
+  )
 
   pkmnImg.innerHTML = "<img src='" + data.sprites.front_default + "' >";
 
@@ -59,17 +60,18 @@ axios.get("https://pokeapi.co/api/v2/pokemon/65/").then((response) => {
 
   let pkmnImg = document.getElementById("my-pkmn2");
 
-  // let alakazam = new Pokemon(
-  //   data.id,
-  //   data.name,
-  //   data.stats[5].base_stat,
-  //   data.stats[4].base_stat,
-  //   data.stats[3].base_stat,
-  //   data.abilities[0].ability.name,
-  //   data.abilities[1].ability.name,
-  //   data.abilities[2].ability.name,
-  //   data.moves[7].move.name,
-  // )
+  let alakazam = new Pokemon(
+    data.id,
+    data.name,
+    data.stats[5].base_stat,
+    data.stats[4].base_stat,
+    data.stats[3].base_stat,
+
+    data.moves[0].move.name,
+    data.moves[1].move.name,
+    data.moves[2].move.name,
+    data.moves[7].move.name,
+  )
 
   pkmnImg.innerHTML = "<img src='" + data.sprites.front_default + "' >";
 
@@ -83,17 +85,18 @@ axios.get("https://pokeapi.co/api/v2/pokemon/143/").then((response) => {
 
   let pkmnImg = document.getElementById("my-pkmn3");
 
-  // let snorlax = new Pokemon(
-  //   data.id,
-  //   data.name,
-  //   data.stats[5].base_stat,
-  //   data.stats[4].base_stat,
-  //   data.stats[3].base_stat,
-  //   data.abilities[0].ability.name,
-  //   data.abilities[1].ability.name,
-  //   data.abilities[2].ability.name,
-  //   data.moves[7].move.name,
-  // )
+  let snorlax = new Pokemon(
+    data.id,
+    data.name,
+    data.stats[5].base_stat,
+    data.stats[4].base_stat,
+    data.stats[3].base_stat,
+
+    data.moves[0].move.name,
+    data.moves[1].move.name,
+    data.moves[2].move.name,
+    data.moves[7].move.name,
+  )
 
   pkmnImg.innerHTML = "<img src='" + data.sprites.front_default + "' >";
 
@@ -107,17 +110,18 @@ axios.get("https://pokeapi.co/api/v2/pokemon/145/").then((response) => {
 
   let pkmnImg = document.getElementById("my-pkmn4");
 
-  // let zapdos = new Pokemon(
-  //   data.id,
-  //   data.name,
-  //   data.stats[5].base_stat,
-  //   data.stats[4].base_stat,
-  //   data.stats[3].base_stat,
-  //   data.abilities[0].ability.name,
-  //   data.abilities[1].ability.name,
-  //   data.abilities[2].ability.name,
-  //   data.moves[7].move.name,
-  // )
+  let zapdos = new Pokemon(
+    data.id,
+    data.name,
+    data.stats[5].base_stat,
+    data.stats[4].base_stat,
+    data.stats[3].base_stat,
+
+    data.moves[0].move.name,
+    data.moves[1].move.name,
+    data.moves[2].move.name,
+    data.moves[7].move.name,
+  )
 
   pkmnImg.innerHTML = "<img src='" + data.sprites.front_default + "' >";
 
@@ -131,17 +135,18 @@ axios.get("https://pokeapi.co/api/v2/pokemon/149/").then((response) => {
 
   let pkmnImg = document.getElementById("my-pkmn5");
 
-  // let dragonite = new Pokemon(
-  //   data.id,
-  //   data.name,
-  //   data.stats[5].base_stat,
-  //   data.stats[4].base_stat,
-  //   data.stats[3].base_stat,
-  //   data.abilities[0].ability.name,
-  //   data.abilities[1].ability.name,
-  //   data.abilities[2].ability.name,
-  //   data.moves[7].move.name,
-  // )
+  let dragonite = new Pokemon(
+    data.id,
+    data.name,
+    data.stats[5].base_stat,
+    data.stats[4].base_stat,
+    data.stats[3].base_stat,
+
+    data.moves[0].move.name,
+    data.moves[1].move.name,
+    data.moves[2].move.name,
+    data.moves[7].move.name,
+  )
 
   pkmnImg.innerHTML = "<img src='" + data.sprites.front_default + "' >";
 
@@ -155,17 +160,18 @@ axios.get("https://pokeapi.co/api/v2/pokemon/3/").then((response) => {
 
   let pkmnImg = document.getElementById("my-pkmn6");
 
-  // let venusaur = new Pokemon(
-  //   data.id,
-  //   data.name,
-  //   data.stats[5].base_stat,
-  //   data.stats[4].base_stat,
-  //   data.stats[3].base_stat,
-  //   data.abilities[0].ability.name,
-  //   data.abilities[1].ability.name,
-  //   data.abilities[2].ability.name,
-  //   data.moves[7].move.name,
-  // )
+  let venusaur = new Pokemon(
+    data.id,
+    data.name,
+    data.stats[5].base_stat,
+    data.stats[4].base_stat,
+    data.stats[3].base_stat,
+
+    data.moves[0].move.name,
+    data.moves[1].move.name,
+    data.moves[2].move.name,
+    data.moves[7].move.name,
+  )
 
   pkmnImg.innerHTML = "<img src='" + data.sprites.front_default + "' >";
 
@@ -173,53 +179,16 @@ axios.get("https://pokeapi.co/api/v2/pokemon/3/").then((response) => {
   console.log(error)
 })
 
-// // Repeat the axios call but for Venusaur
-// // Axios gets the source of the info im trying to relay onto my page.
-// axios.get("https://pokeapi.co/api/v2/pokemon/3/").then((response) => {
 
-//   // Im grabbing a bunch of tags by an id or class that I want to fill with the info from the API.
-//   let data = response.data
 
-//   let index = document.querySelector("#id2")
-//   let name = document.querySelector("#name2")
-//   let hp = document.querySelector("#hp2")
-//   let attack = document.querySelector("#attack2")
-//   let defense = document.querySelector("#defense2")
-//   let abl1 = document.querySelector(".abl1")
-//   let abl2 = document.querySelector(".abl2")
-//   let abl3 = document.querySelector(".abl3")
-//   let abl4 = document.querySelector(".abl4")
 
-//   // Here I ceate a new pokemon to fill in the info in the class Pokemon with API info.
-//   let venusaur = new Pokemon(
-//     // Here I use the info I linked to the class and use it to display it in the HTML file with the querySelector that I grabbed earlier.
-//     data.id,
-//     data.name,
-//     data.stats[5].base_stat,
-//     data.stats[4].base_stat,
-//     data.stats[3].base_stat,
-//     data.abilities[0].ability.name,
-//     data.abilities[1].ability.name,
-//     data.moves[3].move.name,
-//     data.moves[19].move.name,
-//   )
 
-//   // Here I use the info I linked to the class and use it to display it in the HTML file with the querySelector that I grabbed earlier.
-//   index.innerHTML = "#00" + data.id
-//   name.innerHTML = data.name
-//   hp.innerHTML = "HP: " + data.stats[5].base_stat
-//   attack.innerHTML = "ATK: " + data.stats[4].base_stat
-//   defense.innerHTML = "DEF: " + data.stats[3].base_stat
-//   abl1.innerHTML = data.abilities[0].ability.name
-//   abl2.innerHTML = data.abilities[1].ability.name
-//   abl3.innerHTML = data.moves[2].move.name
-//   abl4.innerHTML = data.moves[19].move.name
 
-// }).catch((error) => {
-//   console.log(error)
-// })
 
-// // Repeat once more for Starmie
+
+
+
+
 // // Axios gets the source of the info im trying to relay onto my page.
 // axios.get("https://pokeapi.co/api/v2/pokemon/121/").then((response) => {
 
