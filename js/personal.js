f980dae8b02fb3b10ba748037b0f74588cf7bdc3
Yorvi <jorvinrosario7@gmail.com>
2018-09-20 03:41:28 -0400

    added all pokemon data through ajax calls

--- js/personal.js
+++ js/personal.js
@@ -66,7 +66,7 @@ axios.get("https://raw.githubusercontent.com/Yorvi/api-data/master/data/api/v2/p
     id.innerHTML = "<small>NO. </small>0" + data.id;
     name.innerHTML = data.name.charAt(0).toUpperCase() + data.name.slice(1) + "<small>LV. </small>45";
     hp.innerHTML = "<small>HP </small>" + data.stats[5].base_stat;
-    height.innerHTML = "<small>HT</small> " + Math.floor(feet / 1.5) + "' " + Math.floor(Math.floor(inch) / 20) + '"';
+    height.innerHTML = "<small>HT</small> " + Math.floor(feet / 1.5) + "' " + Math.floor(Math.floor(inch) / 20) + "\"";
     weight.innerHTML = "<small>WT</small> " + Math.floor(Math.floor(pkmnWeight) / 10) + " lbs.";
     abl1.innerHTML = data.moves[10].move.name.charAt(0).toUpperCase() + data.moves[10].move.name.slice(1);
     abl2.innerHTML = data.moves[5].move.name.charAt(0).toUpperCase() + data.moves[5].move.name.slice(1);
@@ -81,24 +81,24 @@ axios.get("https://raw.githubusercontent.com/Yorvi/api-data/master/data/api/v2/p
     console.log(error);
   });
 
-axios
-  .get(
-    "https://raw.githubusercontent.com/Yorvi/api-data/master/data/api/v2/pokemon/65/index.json"
-  )
-  .then(response => {
+axios.get("https://raw.githubusercontent.com/Yorvi/api-data/master/data/api/v2/pokemon/65/index.json").then(response => {
     let data = response.data;
 
-    let id = document.getElementById("");
-    let name = document.getElementById("");
-    let hp = document.getElementById("");
-    let height = document.getElementById("");
-    let weight = document.getElementById("");
-    let abl1 = document.getElementById("");
-    let abl2 = document.getElementById("");
-    let abl3 = document.getElementById("");
-    let abl4 = document.getElementById("");
-    let atk = document.getElementById("");
-    let def = document.getElementById("");
+    let id = document.getElementById("ala-id");
+    let name = document.getElementById("ala-nm");
+    let hp = document.getElementById("ala-hp");
+    let pkmnHeight = data.height * 20;
+    let inch = 0.3937 * pkmnHeight;
+    let feet = 0.0328 * pkmnHeight;
+    let height = document.getElementById("ala-ht");
+    let pkmnWeight = data.weight * 2.2046;
+    let weight = document.getElementById("ala-wt");
+    let abl1 = document.getElementById("ala-abl1");
+    let abl2 = document.getElementById("ala-abl2");
+    let abl3 = document.getElementById("ala-abl3");
+    let abl4 = document.getElementById("ala-abl4");
+    let atk = document.getElementById("ala-atk");
+    let def = document.getElementById("ala-def");
 
     let pkmnImg = document.getElementById("my-pkmn2");
 
@@ -116,24 +116,14 @@ axios
     );
 
     id.innerHTML = "<small>NO. </small>0" + data.id;
-    name.innerHTML = data.name
-        .charAt(0)
-        .toUpperCase() + data.name.slice(1) + "<small>LV. </small>45";
+    name.innerHTML = data.name.charAt(0).toUpperCase() + data.name.slice(1) + "<small>LV. </small>52";
     hp.innerHTML = "<small>HP </small>" + data.stats[5].base_stat;
-    height.innerHTML = "<small>HT</small> " + Math.floor(feet / 1.5) + "' " + Math.floor(Math.floor(inch) / 20) + '"';
+    height.innerHTML = "<small>HT</small> " + Math.floor(feet / 1.5) + "' " + Math.floor(Math.floor(inch) / 20) + "\"";
     weight.innerHTML = "<small>WT</small> " + Math.floor(Math.floor(pkmnWeight) / 10) + " lbs.";
-    abl1.innerHTML = data.moves[10].move.name
-        .charAt(0)
-        .toUpperCase() + data.moves[10].move.name.slice(1);
-    abl2.innerHTML = data.moves[5].move.name
-        .charAt(0)
-        .toUpperCase() + data.moves[5].move.name.slice(1);
-    abl3.innerHTML = data.moves[7].move.name
-        .charAt(0)
-        .toUpperCase() + data.moves[7].move.name.slice(1);
-    abl4.innerHTML = data.moves[18].move.name
-        .charAt(0)
-        .toUpperCase() + data.moves[18].move.name.slice(1);
+    abl1.innerHTML = data.moves[10].move.name.charAt(0).toUpperCase() + data.moves[10].move.name.slice(1);
+    abl2.innerHTML = data.moves[5].move.name.charAt(0).toUpperCase() + data.moves[5].move.name.slice(1);
+    abl3.innerHTML = data.moves[7].move.name.charAt(0).toUpperCase() + data.moves[7].move.name.slice(1);
+    abl4.innerHTML = data.moves[18].move.name.charAt(0).toUpperCase() + data.moves[18].move.name.slice(1);
     atk.innerHTML = "ATK <span>" + data.stats[4].base_stat + "</span>";
     def.innerHTML = "DEF <span>" + data.stats[3].base_stat + "</span>"; 
 
@@ -143,24 +133,24 @@ axios
     console.log(error);
   });
 
-axios
-  .get(
-    "https://raw.githubusercontent.com/Yorvi/api-data/master/data/api/v2/pokemon/143/index.json"
-  )
-  .then(response => {
+axios.get("https://raw.githubusercontent.com/Yorvi/api-data/master/data/api/v2/pokemon/143/index.json").then(response => {
     let data = response.data;
 
-    let id = document.getElementById("");
-    let name = document.getElementById("");
-    let hp = document.getElementById("");
-    let height = document.getElementById("");
-    let weight = document.getElementById("");
-    let abl1 = document.getElementById("");
-    let abl2 = document.getElementById("");
-    let abl3 = document.getElementById("");
-    let abl4 = document.getElementById("");
-    let atk = document.getElementById("");
-    let def = document.getElementById("");
+    let id = document.getElementById("snor-id");
+    let name = document.getElementById("snor-nm");
+    let hp = document.getElementById("snor-hp");
+    let pkmnHeight = data.height * 20;
+    let inch = 0.3937 * pkmnHeight;
+    let feet = 0.0328 * pkmnHeight;
+    let height = document.getElementById("snor-ht");
+    let pkmnWeight = data.weight * 2.2046;
+    let weight = document.getElementById("snor-wt");
+    let abl1 = document.getElementById("snor-abl1");
+    let abl2 = document.getElementById("snor-abl2");
+    let abl3 = document.getElementById("snor-abl3");
+    let abl4 = document.getElementById("snor-abl4");
+    let atk = document.getElementById("snor-atk");
+    let def = document.getElementById("snor-def");
 
     let pkmnImg = document.getElementById("my-pkmn3");
 
@@ -177,25 +167,15 @@ axios
       data.moves[7].move.name
     );
 
-    id.innerHTML = "<small>NO. </small>0" + data.id;
-    name.innerHTML = data.name
-        .charAt(0)
-        .toUpperCase() + data.name.slice(1) + "<small>LV. </small>45";
+    id.innerHTML = "<small>NO. </small>" + data.id;
+    name.innerHTML = data.name.charAt(0).toUpperCase() + data.name.slice(1) + "<small>LV. </small>42";
     hp.innerHTML = "<small>HP </small>" + data.stats[5].base_stat;
-    height.innerHTML = "<small>HT</small> " + Math.floor(feet / 1.5) + "' " + Math.floor(Math.floor(inch) / 20) + '"';
+    height.innerHTML = "<small>HT</small> " + Math.floor(feet / 1.5) + "' " + Math.floor(Math.floor(inch) / 20) + "\"";
     weight.innerHTML = "<small>WT</small> " + Math.floor(Math.floor(pkmnWeight) / 10) + " lbs.";
-    abl1.innerHTML = data.moves[10].move.name
-        .charAt(0)
-        .toUpperCase() + data.moves[10].move.name.slice(1);
-    abl2.innerHTML = data.moves[5].move.name
-        .charAt(0)
-        .toUpperCase() + data.moves[5].move.name.slice(1);
-    abl3.innerHTML = data.moves[7].move.name
-        .charAt(0)
-        .toUpperCase() + data.moves[7].move.name.slice(1);
-    abl4.innerHTML = data.moves[18].move.name
-        .charAt(0)
-        .toUpperCase() + data.moves[18].move.name.slice(1);
+    abl1.innerHTML = data.moves[10].move.name.charAt(0).toUpperCase() + data.moves[10].move.name.slice(1);
+    abl2.innerHTML = data.moves[5].move.name.charAt(0).toUpperCase() + data.moves[5].move.name.slice(1);
+    abl3.innerHTML = data.moves[7].move.name.charAt(0).toUpperCase() + data.moves[7].move.name.slice(1);
+    abl4.innerHTML = data.moves[18].move.name.charAt(0).toUpperCase() + data.moves[18].move.name.slice(1);
     atk.innerHTML = "ATK <span>" + data.stats[4].base_stat + "</span>";
     def.innerHTML = "DEF <span>" + data.stats[3].base_stat + "</span>"; 
 
@@ -205,24 +185,24 @@ axios
     console.log(error);
   });
 
-axios
-  .get(
-    "https://raw.githubusercontent.com/Yorvi/api-data/master/data/api/v2/pokemon/145/index.json"
-  )
-  .then(response => {
+axios.get("https://raw.githubusercontent.com/Yorvi/api-data/master/data/api/v2/pokemon/145/index.json").then(response => {
     let data = response.data;
 
-    let id = document.getElementById("");
-    let name = document.getElementById("");
-    let hp = document.getElementById("");
-    let height = document.getElementById("");
-    let weight = document.getElementById("");
-    let abl1 = document.getElementById("");
-    let abl2 = document.getElementById("");
-    let abl3 = document.getElementById("");
-    let abl4 = document.getElementById("");
-    let atk = document.getElementById("");
-    let def = document.getElementById("");
+    let id = document.getElementById("zap-id");
+    let name = document.getElementById("zap-nm");
+    let hp = document.getElementById("zap-hp");
+    let pkmnHeight = data.height * 20;
+    let inch = 0.3937 * pkmnHeight;
+    let feet = 0.0328 * pkmnHeight;
+    let height = document.getElementById("zap-ht");
+    let pkmnWeight = data.weight * 2.2046;
+    let weight = document.getElementById("zap-wt");
+    let abl1 = document.getElementById("zap-abl1");
+    let abl2 = document.getElementById("zap-abl2");
+    let abl3 = document.getElementById("zap-abl3");
+    let abl4 = document.getElementById("zap-abl4");
+    let atk = document.getElementById("zap-atk");
+    let def = document.getElementById("zap-def");
 
     let pkmnImg = document.getElementById("my-pkmn4");
 
@@ -240,24 +220,14 @@ axios
     );
 
     id.innerHTML = "<small>NO. </small>0" + data.id;
-    name.innerHTML = data.name
-        .charAt(0)
-        .toUpperCase() + data.name.slice(1) + "<small>LV. </small>45";
+    name.innerHTML = data.name.charAt(0).toUpperCase() + data.name.slice(1) + "<small>LV. </small>64";
     hp.innerHTML = "<small>HP </small>" + data.stats[5].base_stat;
-    height.innerHTML = "<small>HT</small> " + Math.floor(feet / 1.5) + "' " + Math.floor(Math.floor(inch) / 20) + '"';
+    height.innerHTML = "<small>HT</small> " + Math.floor(feet / 1.5) + "' " + Math.floor(Math.floor(inch) / 20) + "\"";
     weight.innerHTML = "<small>WT</small> " + Math.floor(Math.floor(pkmnWeight) / 10) + " lbs.";
-    abl1.innerHTML = data.moves[10].move.name
-        .charAt(0)
-        .toUpperCase() + data.moves[10].move.name.slice(1);
-    abl2.innerHTML = data.moves[5].move.name
-        .charAt(0)
-        .toUpperCase() + data.moves[5].move.name.slice(1);
-    abl3.innerHTML = data.moves[7].move.name
-        .charAt(0)
-        .toUpperCase() + data.moves[7].move.name.slice(1);
-    abl4.innerHTML = data.moves[18].move.name
-        .charAt(0)
-        .toUpperCase() + data.moves[18].move.name.slice(1);
+    abl1.innerHTML = data.moves[10].move.name.charAt(0).toUpperCase() + data.moves[10].move.name.slice(1);
+    abl2.innerHTML = data.moves[5].move.name.charAt(0).toUpperCase() + data.moves[5].move.name.slice(1);
+    abl3.innerHTML = data.moves[7].move.name.charAt(0).toUpperCase() + data.moves[7].move.name.slice(1);
+    abl4.innerHTML = data.moves[18].move.name.charAt(0).toUpperCase() + data.moves[18].move.name.slice(1);
     atk.innerHTML = "ATK <span>" + data.stats[4].base_stat + "</span>";
     def.innerHTML = "DEF <span>" + data.stats[3].base_stat + "</span>"; 
 
@@ -274,17 +244,21 @@ axios
   .then(response => {
     let data = response.data;
 
-    let id = document.getElementById("");
-    let name = document.getElementById("");
-    let hp = document.getElementById("");
-    let height = document.getElementById("");
-    let weight = document.getElementById("");
-    let abl1 = document.getElementById("");
-    let abl2 = document.getElementById("");
-    let abl3 = document.getElementById("");
-    let abl4 = document.getElementById("");
-    let atk = document.getElementById("");
-    let def = document.getElementById("");
+    let id = document.getElementById("dragon-id");
+    let name = document.getElementById("dragon-nm");
+    let hp = document.getElementById("dragon-hp");
+    let pkmnHeight = data.height * 20;
+    let inch = 0.3937 * pkmnHeight;
+    let feet = 0.0328 * pkmnHeight;
+    let height = document.getElementById("dragon-ht");
+    let pkmnWeight = data.weight * 2.2046;
+    let weight = document.getElementById("dragon-wt");
+    let abl1 = document.getElementById("dragon-abl1");
+    let abl2 = document.getElementById("dragon-abl2");
+    let abl3 = document.getElementById("dragon-abl3");
+    let abl4 = document.getElementById("dragon-abl4");
+    let atk = document.getElementById("dragon-atk");
+    let def = document.getElementById("dragon-def");
 
     let pkmnImg = document.getElementById("my-pkmn5");
 
@@ -302,24 +276,14 @@ axios
     );
 
     id.innerHTML = "<small>NO. </small>0" + data.id;
-    name.innerHTML = data.name
-        .charAt(0)
-        .toUpperCase() + data.name.slice(1) + "<small>LV. </small>45";
+    name.innerHTML = data.name.charAt(0).toUpperCase() + data.name.slice(1) + "<small>LV. </small>67";
     hp.innerHTML = "<small>HP </small>" + data.stats[5].base_stat;
-    height.innerHTML = "<small>HT</small> " + Math.floor(feet / 1.5) + "' " + Math.floor(Math.floor(inch) / 20) + '"';
+    height.innerHTML = "<small>HT</small> " + Math.floor(feet / 1.5) + "' " + Math.floor(Math.floor(inch) / 20) + "\"";
     weight.innerHTML = "<small>WT</small> " + Math.floor(Math.floor(pkmnWeight) / 10) + " lbs.";
-    abl1.innerHTML = data.moves[10].move.name
-        .charAt(0)
-        .toUpperCase() + data.moves[10].move.name.slice(1);
-    abl2.innerHTML = data.moves[5].move.name
-        .charAt(0)
-        .toUpperCase() + data.moves[5].move.name.slice(1);
-    abl3.innerHTML = data.moves[7].move.name
-        .charAt(0)
-        .toUpperCase() + data.moves[7].move.name.slice(1);
-    abl4.innerHTML = data.moves[18].move.name
-        .charAt(0)
-        .toUpperCase() + data.moves[18].move.name.slice(1);
+    abl1.innerHTML = data.moves[10].move.name.charAt(0).toUpperCase() + data.moves[10].move.name.slice(1);
+    abl2.innerHTML = data.moves[5].move.name.charAt(0).toUpperCase() + data.moves[5].move.name.slice(1);
+    abl3.innerHTML = data.moves[7].move.name.charAt(0).toUpperCase() + data.moves[7].move.name.slice(1);
+    abl4.innerHTML = data.moves[18].move.name.charAt(0).toUpperCase() + data.moves[18].move.name.slice(1);
     atk.innerHTML = "ATK <span>" + data.stats[4].base_stat + "</span>";
     def.innerHTML = "DEF <span>" + data.stats[3].base_stat + "</span>"; 
 
@@ -336,17 +300,21 @@ axios
   .then(response => {
     let data = response.data;
 
-    let id = document.getElementById("");
-    let name = document.getElementById("");
-    let hp = document.getElementById("");
-    let height = document.getElementById("");
-    let weight = document.getElementById("");
-    let abl1 = document.getElementById("");
-    let abl2 = document.getElementById("");
-    let abl3 = document.getElementById("");
-    let abl4 = document.getElementById("");
-    let atk = document.getElementById("");
-    let def = document.getElementById("");
+    let id = document.getElementById("venu-id");
+    let name = document.getElementById("venu-nm");
+    let hp = document.getElementById("venu-hp");
+    let pkmnHeight = data.height * 20;
+    let inch = 0.3937 * pkmnHeight;
+    let feet = 0.0328 * pkmnHeight;
+    let height = document.getElementById("venu-ht");
+    let pkmnWeight = data.weight * 2.2046;
+    let weight = document.getElementById("venu-wt");
+    let abl1 = document.getElementById("venu-abl1");
+    let abl2 = document.getElementById("venu-abl2");
+    let abl3 = document.getElementById("venu-abl3");
+    let abl4 = document.getElementById("venu-abl4");
+    let atk = document.getElementById("venu-atk");
+    let def = document.getElementById("venu-def");
 
     let pkmnImg = document.getElementById("my-pkmn6");
 
@@ -364,24 +332,14 @@ axios
     );
 
     id.innerHTML = "<small>NO. </small>0" + data.id;
-    name.innerHTML = data.name
-        .charAt(0)
-        .toUpperCase() + data.name.slice(1) + "<small>LV. </small>45";
+    name.innerHTML = data.name.charAt(0).toUpperCase() + data.name.slice(1) + "<small>LV. </small>80";
     hp.innerHTML = "<small>HP </small>" + data.stats[5].base_stat;
-    height.innerHTML = "<small>HT</small> " + Math.floor(feet / 1.5) + "' " + Math.floor(Math.floor(inch) / 20) + '"';
+    height.innerHTML = "<small>HT</small> " + Math.floor(feet / 1.5) + "' " + Math.floor(Math.floor(inch) / 20) + "\"";
     weight.innerHTML = "<small>WT</small> " + Math.floor(Math.floor(pkmnWeight) / 10) + " lbs.";
-    abl1.innerHTML = data.moves[10].move.name
-        .charAt(0)
-        .toUpperCase() + data.moves[10].move.name.slice(1);
-    abl2.innerHTML = data.moves[5].move.name
-        .charAt(0)
-        .toUpperCase() + data.moves[5].move.name.slice(1);
-    abl3.innerHTML = data.moves[7].move.name
-        .charAt(0)
-        .toUpperCase() + data.moves[7].move.name.slice(1);
-    abl4.innerHTML = data.moves[18].move.name
-        .charAt(0)
-        .toUpperCase() + data.moves[18].move.name.slice(1);
+    abl1.innerHTML = data.moves[10].move.name.charAt(0).toUpperCase() + data.moves[10].move.name.slice(1);
+    abl2.innerHTML = data.moves[5].move.name.charAt(0).toUpperCase() + data.moves[5].move.name.slice(1);
+    abl3.innerHTML = data.moves[7].move.name.charAt(0).toUpperCase() + data.moves[7].move.name.slice(1);
+    abl4.innerHTML = data.moves[18].move.name.charAt(0).toUpperCase() + data.moves[18].move.name.slice(1);
     atk.innerHTML = "ATK <span>" + data.stats[4].base_stat + "</span>";
     def.innerHTML = "DEF <span>" + data.stats[3].base_stat + "</span>"; 
 
